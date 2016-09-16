0d018e27ae05a3502685b72abf6679ea04efa419
viKuo <lost.angel.fin@gmail.com>
2016-09-16 16:40:26 -0500

    can buy ticket

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -4,4 +4,23 @@ class TicketsController < ApplicationController
 		@movies = Movie.all
 	end
 
+	def create
+		if !credit_card_params.length == 16
+			flash[:error] = "Credit card number invalid"
+			redirect_to :back
+		else
+			Ticket.create!(ticket_params)		
+			flash[:notice] = "You have bought a ticket. Please check your inbox for a receipt."
+			redirect_to root_path
+		end
+	end
+
+	private
+  def ticket_params
+    params.require(:ticket).permit(:showtime_id, :customer)
+	end
+
+	def credit_card_params
+		params.require(:credit_card_number)
+	end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,5 @@
 class Ticket < ApplicationRecord
 	validates :showtime_id, :customer, presence: true
+	validates :customer, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 	belongs_to :showtime
 end
